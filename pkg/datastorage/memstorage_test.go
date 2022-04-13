fa87c4832c818998d40b6769721e71c00f710cc6
vod <dannyphvo@gmail.com>
2022-04-12 23:22:39 -0700

    memstorage test to use testify

--- pkg/datastorage/memstorage_test.go
+++ pkg/datastorage/memstorage_test.go
@@ -3,6 +3,9 @@ package datastorage
 import (
 	"sync"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMemStorage_ImplementsDataStorage(t *testing.T) {
@@ -11,17 +14,10 @@ func TestMemStorage_ImplementsDataStorage(t *testing.T) {
 
 func TestMemStorage_Initialize(t *testing.T) {
 	mem := MemStorage{}.Initialize()
-	if mem == nil {
-		t.Error("returned a nil ptr")
-	}
-
-	if mem.data == nil || len(mem.data) != 0 {
-		t.Error("failed to initialize data map")
-	}
-
-	if mem.rwMu == nil {
-		t.Error("failed to initialize rwMutex")
-	}
+	assert.NotNil(t, mem)
+	assert.NotNil(t, mem.data)
+	assert.NotZero(t, mem.data)
+	assert.NotNil(t, mem.rwMu)
 }
 
 func TestMemStorage_RetrieveData(t *testing.T) {
@@ -31,23 +27,13 @@ func TestMemStorage_RetrieveData(t *testing.T) {
 
 	t.Run("existing data", func(t *testing.T) {
 		data, err := mem.RetrieveData("test")
-		if err != nil {
-			t.Fatalf("received unexpected error: %v", err)
-		}
-
-		if string(testData) != string(data) {
-			t.Errorf(
-				"expected data: %s but got: %s",
-				testData, data,
-			)
-		}
+		require.NoError(t, err)
+		assert.Equal(t, testData, data)
 	})
 
 	t.Run("nonexistent key", func(t *testing.T) {
 		_, err := mem.RetrieveData("foo")
-		if err == nil {
-			t.Error("expected error but got none")
-		}
+		assert.Error(t, err)
 	})
 }
 
@@ -58,71 +44,32 @@ func TestMemStorage_StoreData(t *testing.T) {
 
 	t.Run("first write", func(t *testing.T) {
 		err := mem.StoreData(dataName, testData)
-		if err != nil {
-			t.Fatalf("received unexpected error: %v", err)
-		}
-
-		if len(mem.data) != 1 {
-			t.Errorf(
-				"expected MemStorage data capacity of: %d, but got: %d",
-				1, len(mem.data),
-			)
-		}
+		require.NoError(t, err)
+		assert.Len(t, mem.data, 1)
 
 		data := mem.data[dataName]
-		if string(testData) != string(data) {
-			t.Errorf(
-				"expected data: %s but got: %s",
-				testData, data,
-			)
-		}
+		assert.Equal(t, testData, data)
 	})
 
 	t.Run("overwrite", func(t *testing.T) {
 		testData = []byte("foobar")
 		err := mem.StoreData(dataName, testData)
-		if err != nil {
-			t.Fatalf("received unexpected error: %v", err)
-		}
-
-		if len(mem.data) != 1 {
-			t.Errorf(
-				"expected MemStorage data capacity of: %d, but got: %d",
-				1, len(mem.data),
-			)
-		}
+		require.NoError(t, err)
+		assert.Len(t, mem.data, 1)
 
 		data := mem.data[dataName]
-		if string(testData) != string(data) {
-			t.Errorf(
-				"expected data: %s but got: %s",
-				testData, data,
-			)
-		}
+		assert.Equal(t, testData, data)
 	})
 
 	t.Run("new data", func(t *testing.T) {
 		dataName2 := "test2"
 		testData2 := []byte("qwerty")
 		err := mem.StoreData(dataName2, testData2)
-		if err != nil {
-			t.Fatalf("received unexpected error: %v", err)
-		}
+		require.NoError(t, err)
 
-		if len(mem.data) != 2 {
-			t.Errorf(
-				"expected MemStorage data capacity of: %d, but got: %d",
-				2, len(mem.data),
-			)
-		}
-
+		assert.Len(t, mem.data, 2)
 		data := mem.data[dataName2]
-		if string(testData2) != string(data) {
-			t.Errorf(
-				"expected data: %s but got: %s",
-				testData2, data,
-			)
-		}
+		assert.Equal(t, testData2, data)
 	})
 }
 
@@ -132,30 +79,19 @@ func TestMemStorage_DeleteData(t *testing.T) {
 	testData := []byte("test data")
 
 	err := mem.StoreData(dataName, testData)
-	if err != nil {
-		t.Fatalf("received unexpected error: %v", err)
-	}
+	require.NoError(t, err)
 
 	t.Run("delete existing data", func(t *testing.T) {
 		err = mem.DeleteData(dataName)
-		if err != nil {
-			t.Fatalf("received unexpected error: %v", err)
-		}
+		require.NoError(t, err)
 
-		if len(mem.data) != 0 {
-			t.Error("MemStorage data should be empty")
-		}
+		assert.Len(t, mem.data, 0)
 	})
 
 	t.Run("delete nonexistent data", func(t *testing.T) {
 		err = mem.DeleteData(dataName)
-		if err == nil {
-			t.Error("expected error but got none")
-		}
-
-		if len(mem.data) != 0 {
-			t.Error("MemStorage data should be empty")
-		}
+		assert.Error(t, err)
+		assert.Len(t, mem.data, 0)
 	})
 }
 
