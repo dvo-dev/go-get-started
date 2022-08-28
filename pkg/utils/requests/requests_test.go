c7f1639815107e2b0f8e1588985f8e7d03f92a8e
Alex Ma <alex.ma.806@gmail.com>
2022-08-27 23:31:17 -0500

    refactored tests, cleaned up comments

--- pkg/utils/requests/requests_test.go
+++ pkg/utils/requests/requests_test.go
@@ -10,41 +10,48 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Full test coverage for GetRequest, PostRequest
-// CustomRequest can be covered using two different http methods (e.g. PUT, DELETE)
-// Test can be housed in pkg/utils/requests/requests_test.go
-// Separate test functions for each wrapper tested, use sub tests if needed
-
 func TestRequests_GetRequest(t *testing.T) {
-	getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.Method, http.MethodGet, "Request is not for GET Method")
-		assert.Equal(t, r.URL.Query().Get("param1"), "foobar", "Param values do not match")
+	t.Run("basic", func(t *testing.T) {
+		getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.Method, http.MethodGet, "Request is not for GET Method")
+			assert.Equal(t, r.URL.Query().Get("param1"), "foobar", "Param values do not match")
+		})
+
+		testServer := httptest.NewServer(getTestHandler)
+		params := map[string]string{"param1": "foobar"}
+		_, err := GetRequest(testServer.URL, &params, nil)
+		require.NoError(t, err)
 	})
+	t.Run("nil params", func(t *testing.T) {
+		getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.Method, http.MethodGet, "Request is not for GET Method")
+			err := r.ParseForm()
+			if err != nil {
+				t.Fatalf(
+					"unexpected error occurred: %v",
+					err,
+				)
+			}
+			assert.Empty(t, r.Form)
+		})
 
-	testServer := httptest.NewServer(getTestHandler)
-	params := map[string]string{"param1": "foobar"}
-	_, err := GetRequest(testServer.URL, &params, nil)
-	require.NoError(t, err)
-}
-
-func TestRequests_GetRequest_NilParams(t *testing.T) {
-	getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.Method, http.MethodGet, "Request is not for GET Method")
-		r.ParseForm()
-		assert.Empty(t, r.Form)
+		testServer := httptest.NewServer(getTestHandler)
+		_, err := GetRequest(testServer.URL, nil, nil)
+		require.NoError(t, err)
 	})
-
-	testServer := httptest.NewServer(getTestHandler)
-	_, err := GetRequest(testServer.URL, nil, nil)
-	require.NoError(t, err)
 }
 
 func TestRequests_PostRequest(t *testing.T) {
 	getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, r.Method, http.MethodPost, "Request is not for POST Method")
-		r.ParseForm()
+		err := r.ParseForm()
+		if err != nil {
+			t.Fatalf(
+				"unexpected error occurred: %v",
+				err,
+			)
+		}
 		assert.Empty(t, r.Form)
-		// Check params, header. etc.
 	})
 
 	testServer := httptest.NewServer(getTestHandler)
@@ -56,9 +63,14 @@ func TestRequests_PostRequest(t *testing.T) {
 func TestRequest_CustomRequest(t *testing.T) {
 	getTestHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, r.Method, "custom", "Request is not for custom method")
-		r.ParseForm()
+		err := r.ParseForm()
+		if err != nil {
+			t.Fatalf(
+				"unexpected error occurred: %v",
+				err,
+			)
+		}
 		assert.NotEmpty(t, r.Form)
-		// Check params, header. etc.
 	})
 
 	testServer := httptest.NewServer(getTestHandler)
